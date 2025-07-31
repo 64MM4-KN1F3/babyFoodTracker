2b19ea752856fe7c39396472728bc187d8406a72
64MM4-KN1F3 <aweatherley@gmail.com>
2025-07-31 19:50:26 +1000

    Deploy alpha to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/babyFoodTracker/',
   root: 'src',
   envDir: '../', // <-- Add this line to point to the project root for .env files
   build: {
