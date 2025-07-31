195df633d2bebf63192386a410ca21e646790f9a
64MM4-KN1F3 <aweatherley@gmail.com>
2025-07-31 14:31:43 +1000

    This should prob be deleted later

---
+++ astro-babyfoodtracker/src/data/foods.js
@@ -0,0 +1,14 @@
+export const foodCategories = [
+    {
+        name: 'Fruits',
+        foods: ['Apple', 'Banana', 'Orange']
+    },
+    {
+        name: 'Vegetables',
+        foods: ['Carrot', 'Broccoli', 'Peas']
+    },
+    {
+        name: 'Grains',
+        foods: ['Rice', 'Oats', 'Wheat']
+    }
+];
